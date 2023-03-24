63e7d2d14245b36aed126bbf23ed815dfe377caf
longv1-code <112179922+longv1-code@users.noreply.github.com>
2023-03-24 08:45:36 -0500

    Update mainMenu.cs

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//testing...plz work. hello if you see this. hehe. <3
+//testing...plz work. hello if you see this. hehe. <3 toot toot
 public class mainMenu : MonoBehaviour
 {
     
