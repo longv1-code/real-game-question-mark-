903f6ba6c6b783f170a0a16e498e7e5f31ada9b5
longv1-code <longvo.work84@gmail.com>
2023-03-29 07:04:34 -0500

    what am i doing at this point

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _startingSceneTransition;
+    [SerializeField] private GameObject _endingSceneTransition;
+
+    private void Start()
+    {
+        _startingSceneTransition.SetActive(true);
+        FunctionTimer.Create(DisableStartingSceneTransition, timer:5f);
+    }
+
+    private void DisableStartingSceneTransition()
+    {
+        _startingSceneTransition.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _endingSceneTransition.SetActive(true);
+            FunctionTimer.Create(LoadNextLevel, timer:1.5f);
+            
+        }
+    }
+
+    private static void LoadNextLevel()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
